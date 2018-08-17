41fa8ef4e0986dcac1865108e9e567d777e2e41f
ryandomoslai <ryanwork277@gmail.com>
2018-08-16 22:24:06 -0600

    Added simple code to demo sequential "related artist" calls

--- src/App.js
+++ src/App.js
@@ -13,31 +13,72 @@ class App extends Component {
     }
   }
   componentDidMount() {
-    let accessToken = new URLSearchParams(window.location.search).get('access_token');
-    if (!accessToken)
+    let accessToken = new URLSearchParams(window.location.search).get('access_token')
+    if (accessToken == null) {
       return;
-    fetch('https://api.spotify.com/v1/artists/3WrFJ7ztbogyGnTHbHJFl2/related-artists', {
+    }
+    fetch('https://api.spotify.com/v1/me', {
       headers: { 'Authorization': 'Bearer ' + accessToken }
     }).then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => this.setState({
+        user: {
+          name: data.id,
+          followers: data.followers.total
+        }
+      }))
+    this.searchArtist(accessToken)
+  }
+
+  searchArtist(accessToken) {
+    var artistRecurse = {}
+    var artistNames = []
+    fetch('https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/related-artists', {
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    }).then(response => response.json())
+      .then(data => {
+        artistRecurse = data.artists[0]
+        artistNames[0] = artistRecurse.name
+      }).then( data => {
+        let fetchString =
+          'https://api.spotify.com/v1/artists/' +
+          artistRecurse.id +
+          '/related-artists'
+        console.log(fetchString)
+
+        fetch(fetchString, {
+          headers: { 'Authorization': 'Bearer ' + accessToken }
+        }).then(response => response.json())
+          .then(data => {
+            artistRecurse = data.artists[0]
+            artistNames[1] = artistRecurse.name
+            console.log(artistNames)
+          })
+      })
+
   }
 
   render() {
-    console.log(this.state)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.user.name}'s Playlists</h1>
-          <h1>They have {this.state.user.followers} followers!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={() => {
-          window.location = 'http://localhost:8888/login'
-          }}>Sign in
-        </button>
+        {this.state.user.name === "" ?
+          <div>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Click sign in to login!</h1>
+              <button onClick={() => {
+                window.location = 'http://localhost:8888/login'
+              }}>Sign in
+              </button>
+            </header>
+          </div>
+            : <div>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">{this.state.user.name} is logged in!</h1>
+            <h1>They have {this.state.user.followers} followers!</h1>
+          </header>
+        </div>
+        }
       </div>
     );
   }

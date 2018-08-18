157b93728f7afe9a81fc7d250a51a9b1da251838
ryandomoslai <ryanwork277@gmail.com>
2018-08-18 14:18:25 -0600

    Created a new skeleton class for an ArtistTree, which is a node based tree created to house the chained artist requests

---
+++ src/artistTree.js
@@ -0,0 +1,124 @@
+/**
+ * A node tree, designed for drawing paths from artists in the Spotify API
+ * Created by: Ryan Domoslai, for https://github.com/ryandomoslai/spotify-project
+ */
+class ArtistTree {
+
+    /**
+     * Creates a new ArtistTree
+     * @param {*} rootItem Item that is initialized as root item.
+     * @param {*} leftTree Item initialized as root left subtree.
+     * @param {*} rightTree Item initialized as root right subtree.
+     */
+    constructor(rootItem, leftTree, rightTree) {
+        rootNode = this.createNewNode(rootItem)
+        this.setLeftSubtree(leftTree)
+        this.setRightSubtree(rightTree)
+    }
+
+    /**
+     * Creates a new ArtistNode for the ArtistTree.
+     * @param {*} item The item to be placed in the new node.
+     */
+    createNewNode(item) {
+        return new ArtistNode(item)
+    }
+
+    /**
+     * Checks if the ArtistTree is empty.
+     * @returns Whether or not the rootNode is initialized.
+     */
+    isEmpty() {
+        return !this.rootNode
+    }
+
+    /**
+     * @returns the root node of the ArtistTree.
+     */
+    rootNode() {
+        return this.rootNode
+    }
+
+    /**
+     * Replaces the current rootNode with newNode
+     * @param {*} newNode An ArtistNode to replace this.rootNode.
+     */
+    setRootNode(newNode) {
+        this.rootNode = newNode
+    }
+
+    /**
+     * @returns The item contained within the rootNode.
+     */
+    rootItem() {
+        if (this.isEmpty()) {
+            // Throw exception
+        } else { return this.rootNode.item() }
+    }
+
+    /**
+     * Sets the item contents of the rootNode.
+     * @param {*} item The new contents of the rootNode.
+     */
+    setRootItem(item) {
+        if (this.isEmpty()) {
+            // Throw exception
+        } else { this.rootNode.setItem(item); }
+    }
+
+    /**
+     * @returns The left subtree of the root.
+     */
+    rootLeftSubTree() {
+        if (this.isEmpty()) {
+            // Throw exception
+        } else {
+            result = new ArtistTree(this.rootNode.leftNode(), null. null)
+            return result
+        }
+    }
+
+    /**
+     * @returns The right subtree of the root.
+     */
+    rootRightSubTree() {
+        if (this.isEmpty()) {
+            // Throw exception
+        } else {
+            result = new ArtistTree(this.rootNode.rightNode(), null, null)
+            return result
+        }
+    }
+
+    /**
+     * Sets the new left subtree of the root.
+     * @param {} newTree The tree to replace the current left tree.
+     */
+    setRootLeftTree(newTree) {
+        if (this.isEmpty()) {
+            // Throw exception
+        } else {
+            if (newTree != null) {
+                this.rootNode.setLeftNode(newTree.rootNode())
+            } else {
+                this.rootNode.setLeftNode(null)
+            }
+        }
+    }
+
+    /**
+     * Sets the new right subtree of the root.
+     * @param {*} newTree The tree to replace the current right tree.
+     */
+    setRootSubTree(newTree) {
+        if (this.isEmpty()) {
+            // Throw exception
+        } else {
+            if (newTree != null) {
+                this.rootNode.setRightNode(newTree.rootNode())
+            } else {
+                this.rootNode.setRightNode(null)
+            }
+        }
+    }
+}

812281885d7b8324bb14510075de8222fe8ba265
ryandomoslai <ryanwork277@gmail.com>
2018-08-21 23:13:48 -0600

    Search Bar now displays results below the bar. Limited to 10 entries. Still need to fix errors due to supposed lack of keys

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -5,25 +5,42 @@ class SearchBar extends Component {
     constructor() {
         super()
         this.accessToken = ""
+
+        this.state = {
+            query: {
+                list: []
+            }
+        }
     }
 
     componentDidMount() {
         this.accessToken = new URLSearchParams(window.location.search).get('access_token')
     }
 
-    onTextChange(query) {
-        fetch('https://api.spotify.com/v1/search?q=' + query +
-        '&type=artist&limit=10', {
+    async onTextChange(query) {
+        let queryFetch = await fetch(
+            'https://api.spotify.com/v1/search?q=' + 
+            query + '&type=artist&limit=10', {
             headers: { 'Authorization': 'Bearer ' + this.accessToken }
         }).then(response => response.json())
-          .then(data => console.log(data))
+
+        this.setState({
+            query: {
+                list: queryFetch.artists.items
+            }
+        })
     }
 
     render() {
+
+        const queryList = this.state.query.list.map((query) =>
+            <li>{query.name}</li>
+        )
         return (
             <div>
                 <input type="text" onKeyUp={event =>
                     this.onTextChange(event.target.value)} />
+                <ul>{queryList}</ul>
             </div>
         );
     }
